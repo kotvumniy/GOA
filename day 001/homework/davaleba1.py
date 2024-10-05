74badb50bba02ea0e574a1d8d851131648171abf
Nika <wolfdolf12@gmail.com>
2024-10-05 20:44:47 +0400

    new

---
+++ day 001/homework/davaleba1.py
@@ -0,0 +1,9 @@
+name = "nika"
+surname = "inadze"
+age = 16
+height = 178.9
+weight = 92.3
+hobby = "programming"
+print("hello my name is " + name + " " + surname + "\n I am " + str(age) +  
+      " years old.\n My height is " + str(height) + " cm and my weight " + str(weight) +
+      " kg \n My hobby is " + hobby)
